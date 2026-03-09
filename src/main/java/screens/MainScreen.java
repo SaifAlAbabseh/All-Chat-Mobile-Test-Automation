31e14af7445ef603c1a19e7d6b7f3abd6c98fd6b
SaifAlAbabseh <sababseh97@gmail.com>
2026-03-09 07:27:14 +0300

    test

--- src/main/java/screens/MainScreen.java
+++ src/main/java/screens/MainScreen.java
@@ -2,19 +2,21 @@ package screens;
 
 import helpers.MainHelpers;
 import org.openqa.selenium.By;
-import util.Page;
+import util.Screen;
 
-public class MainScreen extends Page {
+public class MainScreen extends Screen {
 
     private final By profileButton = By.id("com.example.allchat:id/profilePicture"),
                     logoutButton = By.id("com.example.allchat:id/button5");
 
     public void clickOnProfile() {
+        MainHelpers.waitForVisibilityOfElement(5, profileButton);
         MainHelpers.waitForOfElementToBeClickable(3, profileButton);
         findElementBy(profileButton).click();
     }
 
     public void clickOnLogoutButton() {
+        MainHelpers.waitForVisibilityOfElement(5, logoutButton);
         MainHelpers.waitForOfElementToBeClickable(3, logoutButton);
         findElementBy(logoutButton).click();
     }
